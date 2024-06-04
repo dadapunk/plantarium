8f3d27a1d532f2f6c364f7a6aeb994a7a5dce6a6
Sebas <sebastian.v.a@riseup.net>
2024-06-04 16:59:21 +0200

    added simple controller

--- src/plant/plant.controller.ts
+++ src/plant/plant.controller.ts
@@ -6,8 +6,10 @@ import {
   Param,
   Post,
   NotFoundException,
+  Put,
 } from '@nestjs/common';
-import { Plant } from './plant.entity';
+import { Plant } from './entities/plant.entity';
+import { CreatePlantDTO } from './dto/create-plant.dto';
 
 @Controller({ path: 'plant' })
 export class PlantController {
@@ -21,9 +23,8 @@ export class PlantController {
 
   @Get(':id')
   findPlantById(@Param('id') id: number): Plant {
-    console.log('id', id);
-    const numId = Number(id);
-    const plant = this.plants.find((item) => item.id === id);
+    // Use a ParseIntPipe
+    const plant = this.plants.find((item) => item.id == id);
     if (!plant) {
       throw new NotFoundException('Plant not found');
     }
@@ -36,9 +37,33 @@ export class PlantController {
     body.id = this.idCounter++;
     this.plants.push(body);
   }
+  @Post()
+  async createPlant(@Body() createPlantDTO: CreatePlantDTO) {
+    return 'This action adds a new plant';
+  }
 
-  @Delete('delete:id')
+  @Delete('delete/:id')
   removePlant(@Param('id') id: number): void {
-    this.plants = this.plants.filter((item) => item.id !== id);
+    console.log(`Attempting to delete plant with id: ${id}`);
+    const initialLength = this.plants.length;
+    this.plants = this.plants.filter((item) => item.id != id);
+    if (this.plants.length === initialLength) {
+      console.log(`No plant found with id: ${id}`);
+    } else {
+      console.log(`Deleted plant with id: ${id}`);
+    }
+  }
+  @Put('update/:id')
+  updatePlant(@Param('id') id: number, @Body() body: Plant): void {
+    console.log(`Attempting to update plant with id: ${id}`);
+    const plant = this.plants.find((item) => item.id == id);
+    if (!plant) {
+      throw new NotFoundException('Plant not found');
+    }
+    plant.name = body.name;
+    plant.description = body.description;
+    plant.imageUrl = body.imageUrl;
+    plant.stock = body.stock;
+    console.log(`Updated plant with id: ${id}`);
   }
 }
