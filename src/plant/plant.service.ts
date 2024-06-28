f093ce7cfbbe56cbeaaf880651a6a662fcc8579b
Sebas <sebastian.v.a@riseup.net>
2024-06-28 11:28:41 +0200

    refactor PlantService to use TypeORM repository for CRUD operations

--- src/plant/plant.service.ts
+++ src/plant/plant.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Plant } from './entities/plant.entity';
 import { CreatePlantDTO } from './dto/create-plant.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PlantService {
+  constructor(
+    @InjectRepository(Plant)
+    private plantRepository: Repository<Plant>,
+  ) {}
+
   plants: Plant[] = [];
   idCounter = 1;
 
-  create(createPlantDTO: CreatePlantDTO) {
+  async create(createPlantDTO: CreatePlantDTO): Promise<Plant> {
+    const plant = this.plantRepository.create(createPlantDTO);
+    return this.plantRepository.save(plant);
+  }
+
+  findAll(): Plant[] {
     return this.plants;
   }
-  findAll() {
-    return this.plants;
-  }
-  findOne(id: number) {
-    const plant = this.plants.find((item) => item.id == id);
+
+  findOne(id: number): Plant {
+    const plant = this.plants.find((item) => item.id === id);
+    if (!plant) {
+      throw new NotFoundException(`Plant with ID ${id} not found`);
+    }
     return plant;
   }
-  update(id: number, updatePlantDTO: CreatePlantDTO) {
-    return 'This action updates a #${id} plant';
+
+  update(id: number, updatePlantDTO: CreatePlantDTO): Plant {
+    const plantIndex = this.plants.findIndex((item) => item.id === id);
+    if (plantIndex === -1) {
+      throw new NotFoundException(`Plant with ID ${id} not found`);
+    }
+    this.plants[plantIndex] = { ...this.plants[plantIndex], ...updatePlantDTO };
+    return this.plants[plantIndex];
   }
-  remove(id: number) {
-    return 'This action removes a #${id} plant';
+
+  remove(id: number): void {
+    const plantIndex = this.plants.findIndex((item) => item.id === id);
+    if (plantIndex === -1) {
+      throw new NotFoundException(`Plant with ID ${id} not found`);
+    }
+    this.plants.splice(plantIndex, 1);
   }
 }
