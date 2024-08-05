51d35b8802144f15f93f3d05714ac94beefde9ab
Sebas <sebastian.v.a@riseup.net>
2024-08-05 09:50:00 +0200

    feat: Update ExternalPlantDTO with validation decorators
    The code changes in `external-plant.dto.ts` add validation decorators to the properties of the `ExternalPlantDTO` class. This ensures that the required fields are not empty and that the data types are correct.

--- src/plant/dto/external-plant.dto.ts
+++ src/plant/dto/external-plant.dto.ts
@@ -1,17 +1,69 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsOptional,
+  IsArray,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class ExternalPlantDTO {
+  @IsNumber()
+  @IsNotEmpty()
   id: number;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   slug: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
   created_at: string;
+
+  @IsString()
+  @IsNotEmpty()
   updated_at: string;
+
+  @IsString()
+  @IsNotEmpty()
   scientific_name: string;
+
+  @IsNumber()
+  @IsOptional()
   parent_id: number | null;
+
+  @IsNumber()
+  @IsNotEmpty()
   adopter_id: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   version: number;
+
+  @IsString()
+  @IsNotEmpty()
   type: string;
+
+  @IsString()
+  @IsNotEmpty()
   link: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Object)
   data: Array<{ key: string; value: string }>;
+
+  @ValidateNested()
+  @Type(() => Object)
   images: { thumb: string; title: string };
 
   constructor(json: any) {
